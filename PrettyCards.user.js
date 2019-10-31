b3406dfcf0ca99d3c7ca014989d78a5bc06be017
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-10-31 21:34:53 +0200

    Attempting to split code.

--- PrettyCards.user.js
+++ PrettyCards.user.js
@@ -7,11 +7,21 @@
 // @match        https://*.undercards.net/*
 // @exclude      https://*.undercards.net/*/*
 // @grant        none
-// @require      
-// @require      https://gist.githubusercontent.com/CMD-God/31d8750a69acba2b75a727fc344a25be/raw/TextLibrary.js
+// @require      https://raw.githubusercontent.com/CMD-God/prettycards/master/Utility.js
+// @require      https://raw.githubusercontent.com/CMD-God/prettycards/master/TextLibrary.js
 // ==/UserScript==
 
 //https://media.discordapp.net/attachments/147063257436258305/637627137276510208/y47orgr08su31.png <------ FACTS!
+
+function openPageSpecific() {
+	var length = location.pathname.length, temp;
+	if ((temp = location.pathname.indexOf(".")) === -1 && (temp = location.pathname.indexOf("/")) === -1) {
+		temp = null;
+	}
+	loadScript("https://raw.githubusercontent.com/CMD-God/prettycards/master/PageSpecific/" + location.pathname.substring(1, temp || length) + ".js");
+}
+
+openPageSpecific();
 
 
 onPage("Play", function() {
