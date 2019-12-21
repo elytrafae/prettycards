dab502882ebc6e15fdc3ec564b2539804d59c864
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-21 22:04:58 +0200

    Making progress towards writing . . .

--- TextLibrary.js
+++ TextLibrary.js
@@ -1,20 +1,46 @@
 
+GM_addStyle(".dialogueContainer {position: absolute; bottom: 3em; min-width: 20em; max-width: 100em; width: 60%; left: 20%; padding-top: 14%; background-color: black; border: 5px solid white; z-index: 200;}");
+GM_addStyle(".dialogueTxtContainer {position: absolute; top: 0; left: 0; bottom: 0; right: 0; padding: 0.5em; font-size: 2.5em; color: white;}");
+
 var TextLibrary = {};
 
-TextLibrary.CreateText = function() {
+TextLibrary.CreateText = function(txt) {
     var self = this;
-	this.wait = 200;
+	this.wait = 100/3;
+    this.txt = txt;
+    this.currLetter = 0;
+    this.currLine = 0;
+
+    this.container = document.createElement("div");
+    this.container.className = "dialogueContainer";
+    this.txtcontainer = document.createElement("div");
+    this.txtcontainer.className = "dialogueTxtContainer";
+    this.container.appendChild(this.txtcontainer);
+    this.current_span = document.createElement("span");
+    this.txtcontainer.appendChild(this.current_span);
+    document.body.appendChild(this.container);
+
 	this.timeout = setTimeout(function() {
-        console.log('setTimout this=', this);
         self._typeText();
-    }, 200);
-    this._typeText();
+    }, this.wait);
+    //this._typeText();
 	console.log(this);
 }
 
 TextLibrary.CreateText.prototype._typeText = function() {
-		console.log("TypeText: ", this);
-	};
+	//console.log("TypeText: ", this);
+
+    this.current_span.innerHTML += this.txt[this.currLine].charAt(this.currLetter);
+    this.currLetter++;
+
+    if (this.currLetter < this.txt[this.currLine].length) {
+        //console.log("CurrentLetter: ", this.currLetter, " TextLength: ", this.txt[this.currLine].length);
+        var self = this;
+        this.timeout = setTimeout(function() {
+            self._typeText();
+        }, this.wait);
+    }
+};
 
 TextLibrary.SetText = function() {
 
