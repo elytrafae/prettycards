f334601b838247dff2afee0970ca386d2f34b0d6
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-21 21:04:59 +0200

    Fixed start of TextLibrary

--- PageSpecific/Artifacts.js
+++ PageSpecific/Artifacts.js
@@ -1,5 +1,7 @@
+var dialogue, dialogue2;
 
 onPage("Artifacts", function() {
 	dialogue = new TextLibrary.CreateText();
-	
+    dialogue2 = new TextLibrary.CreateText();
+	console.log("Dialogue: ", dialogue);
 });
--- TextLibrary.js
+++ TextLibrary.js
@@ -2,15 +2,20 @@
 var TextLibrary = {};
 
 TextLibrary.CreateText = function() {
+    var self = this;
 	this.wait = 200;
-	this.timeout = setTimeout(TextLibrary._typeText, this.wait);
+	this.timeout = setTimeout(function() {
+        console.log('setTimout this=', this);
+        self._typeText();
+    }, 200);
+    this._typeText();
 	console.log(this);
 }
 
-TextLibrary._typeText = function() {
-	console.log(this);
-}
+TextLibrary.CreateText.prototype._typeText = function() {
+		console.log("TypeText: ", this);
+	};
 
 TextLibrary.SetText = function() {
-	
+
 }
