bcfdff52c3278d9c26af4b95be2174ecf3c184e4
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-22 10:45:12 +0200

    Start of control implementation.

--- PageSpecific/Artifacts.js
+++ PageSpecific/Artifacts.js
@@ -1,7 +1,7 @@
 var dialogue, dialogue2;
 
 onPage("Artifacts", function() {
-	dialogue = new TextLibrary.CreateText();
-    dialogue2 = new TextLibrary.CreateText();
-	console.log("Dialogue: ", dialogue);
+	dialogue = new TextLibrary.CreateText(["Hello! This line is reeeeeaaaallllllyyyy long, don't you think? [color:#ff0000]Perfect to skip!", "[color:#ff0000]How are you?"]);
+    //dialogue2 = new TextLibrary.CreateText();
+	//console.log("Dialogue: ", dialogue);
 });
--- TextLibrary.js
+++ TextLibrary.js
@@ -13,32 +13,61 @@ TextLibrary.CreateText = function(txt) {
 
     this.container = document.createElement("div");
     this.container.className = "dialogueContainer";
+    this.container.addEventListener("click", function() {self._handleClick();});
+    this.container.tabIndex = 0;
+    this.container.addEventListener("keydown", function(event) {self._handleKeyDown(event)});
     this.txtcontainer = document.createElement("div");
     this.txtcontainer.className = "dialogueTxtContainer";
     this.container.appendChild(this.txtcontainer);
     this.current_span = document.createElement("span");
     this.txtcontainer.appendChild(this.current_span);
     document.body.appendChild(this.container);
+    this.container.focus();
 
 	this.timeout = setTimeout(function() {
-        self._typeText();
+        self._typeText(true);
     }, this.wait);
     //this._typeText();
 	console.log(this);
 }
 
-TextLibrary.CreateText.prototype._typeText = function() {
-	//console.log("TypeText: ", this);
+TextLibrary.CreateText.prototype.CheckLineGo = function() {
+    return (this.currLetter < this.txt[this.currLine].length);
+}
 
-    this.current_span.innerHTML += this.txt[this.currLine].charAt(this.currLetter);
+TextLibrary.CreateText.prototype._typeText = function(makeTimeout) {
+	//console.log("TypeText: ", this);
+    if (this.txt[this.currLine].charAt(this.currLetter) != "[") {
+        this.current_span.innerHTML += this.txt[this.currLine].charAt(this.currLetter);
+    }
     this.currLetter++;
 
-    if (this.currLetter < this.txt[this.currLine].length) {
+    if (this.CheckLineGo() && makeTimeout) {
         //console.log("CurrentLetter: ", this.currLetter, " TextLength: ", this.txt[this.currLine].length);
         var self = this;
         this.timeout = setTimeout(function() {
-            self._typeText();
+            self._typeText(true);
         }, this.wait);
+    }
+};
+
+TextLibrary.CreateText.prototype._handleClick = function() {
+    if (this.CheckLineGo()) {
+        this._skip();
+    } else {
+        console.log("End of line!");
+    }
+};
+
+TextLibrary.CreateText.prototype._handleKeyDown = function(event) {
+    console.log(this , " keyDown! ::", event);
+};
+
+TextLibrary.CreateText.prototype._skip = function() {
+    //console.log(this , " clicked!");
+    clearTimeout(this.timeout);
+    while (this.CheckLineGo()) {
+        this._typeText(false);
     }
 };
 
