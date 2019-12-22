bd39f5e61d121f4e9cabd03f873bd3716279dc47
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-22 11:29:29 +0200

    Controls Implemented!

--- TextLibrary.js
+++ TextLibrary.js
@@ -10,6 +10,7 @@ TextLibrary.CreateText = function(txt) {
     this.txt = txt;
     this.currLetter = 0;
     this.currLine = 0;
+    this.isactive = true;
 
     this.container = document.createElement("div");
     this.container.className = "dialogueContainer";
@@ -19,20 +20,29 @@ TextLibrary.CreateText = function(txt) {
     this.txtcontainer = document.createElement("div");
     this.txtcontainer.className = "dialogueTxtContainer";
     this.container.appendChild(this.txtcontainer);
-    this.current_span = document.createElement("span");
-    this.txtcontainer.appendChild(this.current_span);
+    //this.current_span = document.createElement("span");
+    //this.txtcontainer.appendChild(this.current_span);
+    this._newSpan();
     document.body.appendChild(this.container);
     this.container.focus();
 
-	this.timeout = setTimeout(function() {
-        self._typeText(true);
-    }, this.wait);
+	this._createTimeout();
     //this._typeText();
-	console.log(this);
+	//console.log(this);
 }
 
 TextLibrary.CreateText.prototype.CheckLineGo = function() {
     return (this.currLetter < this.txt[this.currLine].length);
+}
+
+TextLibrary.CreateText.prototype.CheckTextGo = function() {
+    return this.currLine < this.txt.length;
+}
+
+TextLibrary.CreateText.prototype.Remove = function() {
+    clearTimeout(this.timeout);
+    this.container.remove();
+    this.isactive = false;
 }
 
 TextLibrary.CreateText.prototype._typeText = function(makeTimeout) {
@@ -44,10 +54,7 @@ TextLibrary.CreateText.prototype._typeText = function(makeTimeout) {
 
     if (this.CheckLineGo() && makeTimeout) {
         //console.log("CurrentLetter: ", this.currLetter, " TextLength: ", this.txt[this.currLine].length);
-        var self = this;
-        this.timeout = setTimeout(function() {
-            self._typeText(true);
-        }, this.wait);
+        this._createTimeout();
     }
 };
 
@@ -55,13 +62,25 @@ TextLibrary.CreateText.prototype._handleClick = function() {
     if (this.CheckLineGo()) {
         this._skip();
     } else {
-        console.log("End of line!");
+        //console.log("End of line!");
+        this._nextLine();
     }
 };
 
 TextLibrary.CreateText.prototype._handleKeyDown = function(event) {
-    console.log(this , " keyDown! ::", event);
+    //console.log(this , " keyDown! ::", event);
+    console.log("LineGo: ", this.CheckLineGo());
+    if (((event.key == "Shift") || (event.key == "x")) && this.CheckLineGo()) {
+        this._skip();
+    } else if (((event.key == "Enter") || (event.key == "z")) && !this.CheckLineGo()) {
+        this._nextLine();
+    }
 };
+
+TextLibrary.CreateText.prototype._newSpan = function() {
+    this.current_span = document.createElement("span");
+    this.txtcontainer.appendChild(this.current_span);
+}
 
 TextLibrary.CreateText.prototype._skip = function() {
     //console.log(this , " clicked!");
@@ -70,6 +89,26 @@ TextLibrary.CreateText.prototype._skip = function() {
         this._typeText(false);
     }
 };
+
+TextLibrary.CreateText.prototype._nextLine = function() {
+    this.currLetter = 0;
+    this.currLine++;
+    //console.log("this = ", this);
+    if (this.CheckTextGo()) {
+        this.txtcontainer.innerHTML = "";
+        this._newSpan();
+        this._createTimeout();
+    } else {
+        this.Remove();
+    }
+};
+
+TextLibrary.CreateText.prototype._createTimeout = function() {
+    var self = this;
+    this.timeout = setTimeout(function() {
+        self._typeText(true);
+    }, this.wait);
+}
 
 TextLibrary.SetText = function() {
 
