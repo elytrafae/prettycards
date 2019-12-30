f01ead78502d14caab0f1ca2f65826eb7305d198
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-30 11:00:05 +0200

    Update TextLibrary.js

--- TextLibrary.js
+++ TextLibrary.js
@@ -7,6 +7,8 @@ var TextLibrary = {};
 TextLibrary.CreateText = function(txt) {
     var self = this;
 	this.wait = 100/3;
+    this.spd = 1;
+    this.w = 0;
     this.txt = txt;
     this.currLetter = 0;
     this.currLine = 0;
@@ -47,33 +49,40 @@ TextLibrary.CreateText.prototype.Remove = function() {
 
 TextLibrary.CreateText.prototype._typeText = function(makeTimeout) {
 	//console.log("TypeText: ", this);
-    if (this.txt[this.currLine].charAt(this.currLetter) != "[") {
-        this.current_span.innerHTML += this.txt[this.currLine].charAt(this.currLetter);
-    } else {
-        this._handleTextCommand();
+    var i = 0;
+    var line = this.currLine;
+    while ((i < this.spd) && this.CheckLineGo()) {
+        this.w = 0;
+        if (this.txt[this.currLine].charAt(this.currLetter) != "[") {
+            this.current_span.innerHTML += this.txt[this.currLine].charAt(this.currLetter);
+        } else {
+            this._handleTextCommand();
+        }
+        if (line == this.currLine) {this.currLetter++};
+        i++;
     }
-    this.currLetter++;
 
     if (this.CheckLineGo() && makeTimeout) {
         //console.log("CurrentLetter: ", this.currLetter, " TextLength: ", this.txt[this.currLine].length);
-        this._createTimeout();
+        this._createTimeout(this.w);
     }
 };
 
 TextLibrary.CreateText.prototype._handleTextCommand = function() {
-    var c = this.txt[this.currLine].indexOf("]");
+    var c = this.txt[this.currLine].indexOf("]", this.currLetter);
     var scr = this.txt[this.currLine].slice(this.currLetter+1, c);
 	var dd = scr.indexOf(":");
-	if (dd === -1) {
-		
-	} else {
-		var cmd = scr.slice(0, dd-1);
-		var arg = scr.slice(dd+1, scr.length-1);
-		console.log("CMD: ", cmd, " ARG: ", arg);
-	}
-    console.log(scr);
-
     this.currLetter = c;
+	if (dd === -1) {
+        console.log("CMD: ", scr, " ARG: null");
+		this._TextCommands[scr](this);
+	} else {
+		var cmd = scr.slice(0, dd);
+		var arg = scr.slice(dd+1, scr.length);
+		console.log("CMD: ", cmd, " ARG: ", arg);
+        this._TextCommands[cmd](this, arg);
+	}
+    //console.log(scr);
 }
 
 TextLibrary.CreateText.prototype._handleClick = function() {
@@ -87,7 +96,7 @@ TextLibrary.CreateText.prototype._handleClick = function() {
 
 TextLibrary.CreateText.prototype._handleKeyDown = function(event) {
     //console.log(this , " keyDown! ::", event);
-    console.log("LineGo: ", this.CheckLineGo());
+    //console.log("LineGo: ", this.CheckLineGo());
     if (((event.key == "Shift") || (event.key == "x")) && this.CheckLineGo()) {
         this._skip();
     } else if (((event.key == "Enter") || (event.key == "z")) && !this.CheckLineGo()) {
@@ -121,11 +130,73 @@ TextLibrary.CreateText.prototype._nextLine = function() {
     }
 };
 
-TextLibrary.CreateText.prototype._createTimeout = function() {
+TextLibrary.CreateText.prototype._createTimeout = function(plus_wait) {
+    var w = plus_wait || 0;
     var self = this;
     this.timeout = setTimeout(function() {
         self._typeText(true);
-    }, this.wait);
+    }, (this.wait + w));
+}
+
+TextLibrary.CreateText.prototype._TextCommands = {}
+
+TextLibrary.CreateText.prototype._TextCommands.color = function(txt, arg) {
+    if (txt.current_span.innerHTML.length > 0) {
+        var style = txt.current_span.style;
+        txt._newSpan();
+        txt.current_span.style = style;
+    }
+    if (arg == "rainbow") {
+        txt.current_span.style.color = null;
+        txt.current_span.className = txt.current_span.className += " rainbowText"
+    } else {
+        txt.current_span.style.color = arg;
+    }
+}
+
+TextLibrary.CreateText.prototype._TextCommands.font = function(txt, arg) {
+    if (txt.current_span.innerHTML.length > 0) {
+        var style = txt.current_span.style;
+        txt._newSpan();
+        txt.current_span.style = style;
+    }
+    txt.current_span.style["font-family"] = arg;
+}
+
+TextLibrary.CreateText.prototype._TextCommands.size = function(txt, arg) {
+    if (txt.current_span.innerHTML.length > 0) {
+        var style = txt.current_span.style;
+        txt._newSpan();
+        txt.current_span.style = style;
+    }
+    txt.current_span.style["font-size"] = arg;
+}
+
+TextLibrary.CreateText.prototype._TextCommands.waitall = function(txt, arg) {
+    txt.wait = arg;
+}
+
+TextLibrary.CreateText.prototype._TextCommands.speed = function(txt, arg) {
+    txt.spd = arg;
+}
+
+TextLibrary.CreateText.prototype._TextCommands.w = function(txt, arg) {
+    txt.w = arg;
+}
+
+TextLibrary.CreateText.prototype._TextCommands.instant = function(txt) {
+    txt.currLetter++;
+    txt._skip();
+}
+
+TextLibrary.CreateText.prototype._TextCommands.next = function(txt) {
+    txt._nextLine();
+}
+
+TextLibrary.CreateText.prototype._TextCommands.letter = function(txt, arg) {
+    for (var i=0; i < arg; i++) {
+        txt._typeText();
+    }
 }
 
 TextLibrary.SetText = function() {
