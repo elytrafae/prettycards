5908fd297f42d09509b5719c30b2c7608d5fcf7b
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-30 16:52:18 +0200

    SANS UNDERTALE!?!?!?!

--- CardIntroSongManager.js
+++ CardIntroSongManager.js
@@ -1,15 +1,17 @@
 
-var numberOfSongsCardsHave = { //Howe many Jingles does a Card have in my Git Folder?
+var numberOfSongsCardsHave = { //How many Jingles does a Card have in my Git Folder?
 	Chara : 3,
-	Asgore : 3
+	Asgore : 3,
+	Sans : 5
 };
 
 var israndom = false; //Sould the Jingle be selected randomly or based on how many times was the card played?
 var nums = {}; //This counts how many times a given Card's Jingle has been played!
 
 function ProcessJingle(card) {
-	var name = card.name;
-	if (typeof(nums[name]) != "number") {
+	var name = card.card_data.name;
+	console.log("Card Name", name);
+	/*if (typeof(nums[name]) != "number") {
 		for (let key in numberOfSongsCardsHave) {
 			if (key == name) {
 				nums[name] = (nums[name] || 0) + 1;
@@ -19,7 +21,22 @@ function ProcessJingle(card) {
 		if (typeof(nums[name]) != "number") {console.log("ProcessJingle returned!");return;}
 	} else {
 		nums[name]++;
+	}*/
+	if ((numberOfSongsCardsHave[name]) && numberOfSongsCardsHave[name] > 0) {
+		nums[name] = (nums[name] || 0) + 1;
+	} else {
+		console.log("ProcessJingle returned!");
+		return;
 	}
 	console.log(name, nums[name]);
-	
+	var song_nr = (israndom ? getRandomInt(1, numberOfSongsCardsHave[name]) : Math.min(nums[name], numberOfSongsCardsHave[name]))
+	PlaySFX("https://raw.githubusercontent.com/CMD-God/prettycards/master/audio/cards/" + name + "/intro_" + song_nr + ".ogg");
+}
+
+function PlaySFX(url) {
+	var e = document.createElement("audio");
+	e.src = url;
+	e.onended = e.onerror = function(e) {e.target.remove();};
+	document.head.appendChild(e);
+	e.autoplay = true;
 }
