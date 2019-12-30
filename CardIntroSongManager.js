dc87ef920850770572ae8ce9c1860f5ec7b70977
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-30 19:00:18 +0200

    Testing Jingles from the original Database

--- CardIntroSongManager.js
+++ CardIntroSongManager.js
@@ -3,7 +3,8 @@ var numberOfSongsCardsHave = { //How many Jingles does a Card have in my Git Fol
 	Chara : 3,
 	Asgore : 3,
 	Sans : 5,
-	"The Heroine" : 5
+	"The Heroine" : 5,
+	Susie : 2
 };
 
 var israndom = true; //Sould the Jingle be selected randomly or based on how many times was the card played?
@@ -27,11 +28,12 @@ function ProcessJingle(card) {
 		nums[name] = (nums[name] || 0) + 1;
 	} else {
 		console.log("ProcessJingle returned!");
-		return;
+		return false;
 	}
 	console.log(name, nums[name]);
 	var song_nr = (israndom ? getRandomInt(1, numberOfSongsCardsHave[name]) : ((nums[name]-1) % numberOfSongsCardsHave[name])+1)
-	PlaySFX("https://raw.githubusercontent.com/CMD-God/prettycards/master/audio/cards/" + name + "/intro_" + song_nr + ".ogg");
+	card.card_Jingle = PlaySFX("https://raw.githubusercontent.com/CMD-God/prettycards/master/audio/cards/" + name + "/intro_" + song_nr + ".ogg");
+	return true;
 }
 
 function PlaySFX(url) {
@@ -40,4 +42,5 @@ function PlaySFX(url) {
 	e.onended = e.onerror = function(e) {e.target.remove();};
 	document.head.appendChild(e);
 	e.autoplay = true;
+	return e;
 }
