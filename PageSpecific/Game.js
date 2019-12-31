b9eeda599d655a9d2b12a2008041466e1cc1188f
CMD-God <48065803+CMD-God@users.noreply.github.com>
2019-12-31 20:46:35 +0200

    Fixed a bug

--- PageSpecific/Game.js
+++ PageSpecific/Game.js
@@ -2,7 +2,7 @@
 onPage("Game", function() {
 	var infos = document.querySelectorAll("[data-soul]");
 	console.log(infos);
-	for (int i=0; i < infos.length; i++) {
+	for (var i=0; i < infos.length; i++) {
 		var info = infos[i];
 		setTimeout( function() {
 			AddSoulHover(info, info.getAttribute("data-soul"));
