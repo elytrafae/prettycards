ed5d805b62ff01a12d891af736c384e839f66173
CMD-God <48065803+CMD-God@users.noreply.github.com>
2020-01-01 14:56:32 +0200

    More fixes!

--- PageSpecific/GamesList.js
+++ PageSpecific/GamesList.js
@@ -1,7 +1,7 @@
 
 onPage("GamesList", function () {
 	var soulSelect = document.getElementById("customDecks");
-    var div = CustomizeSoulSelectObj(soulSelect, storage.getItem("customDeck"));
+    var div = CustomizeSoulSelectObj(soulSelect, localStorage.getItem("customDeck"));
 	div.style.display = "inline-block";
     //document.getElementById("soulInfo").style.display = "none";
 })
--- PageSpecific/Play.js
+++ PageSpecific/Play.js
@@ -1,7 +1,7 @@
 
 onPage("Play", function () {
 	var soulSelect = document.getElementById("playDecks");
-    var div = CustomizeSoulSelectObj(soulSelect, storage.getItem("playDeck"));
+    var div = CustomizeSoulSelectObj(soulSelect, localStorage.getItem("playDeck"));
 	div.style.display = "inline-block";
     //document.getElementById("soulInfo").style.display = "none";
 })
