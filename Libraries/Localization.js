db2f1aff369e4a9f8f9d2855ab5fbebe91f3499e
CMD-God <48065803+CMD-God@users.noreply.github.com>
2020-01-10 20:35:32 +0200

    Nothing to see here!

---
+++ Libraries/Localization.js
@@ -0,0 +1,7 @@
+
+$.i18n().load( {
+	en: {
+			'card-name-2000': 'Chibi Sayori',
+			'card-2000': '{{KW:MAGIC}}: All {{TRIBE:CHIBI|2}} get +1/+1/+1 for the rest of the game, and clear all negative effects from all ally monsters.'
+		}
+} );
