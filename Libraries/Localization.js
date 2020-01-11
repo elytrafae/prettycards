928aeed12619ecab14791f9021938158b3ed6ed3
CMD-God <48065803+CMD-God@users.noreply.github.com>
2020-01-11 13:11:22 +0200

    Update Localization.js

--- Libraries/Localization.js
+++ Libraries/Localization.js
@@ -7,3 +7,9 @@ $.i18n().load( {
 			['card-'+ (customCardsStart)] : '{{KW:MAGIC}}: All {{TRIBE:CHIBI|2}} get +1/+1/+1 for the rest of the game, and clear all negative effects from all ally monsters.'
 		}
 } );
+
+$.i18n().load( {
+	en: {
+			'tribe-chibi' : '{{PLURAL:$1|Chibi|Chibis}}'
+		}
+} );
