95f3987327e6cb43d80b1b0c9e03c14374adf098
CMD-God <48065803+CMD-God@users.noreply.github.com>
2020-01-24 13:12:13 +0200

    Images For Chibi Cards

--- Libraries/Localization.js
+++ Libraries/Localization.js
@@ -6,7 +6,11 @@ $.i18n().load( {
 			['card-name-' + (customCardsStart)] : 'Chibi Sayori',
 			['card-'+ (customCardsStart)] : '{{KW:MAGIC}}: All {{TRIBE:CHIBI|2}} get +1/+1/+1 for the rest of the game, and clear all negative effects from all ally monsters.',
 			['card-name-' + (customCardsStart+1)] : 'Chibi Natsuki',
-			['card-'+ (customCardsStart+1)] : '{{KW:MAGIC}}: All {{TRIBE:CHIBI|2}} get +1/+1/+1 for the rest of the game, and give +2 {{HP}} to an ally non-{{TRIBE:CHIBI|1}} monster.'
+			['card-'+ (customCardsStart+1)] : '{{KW:MAGIC}}: All {{TRIBE:CHIBI|2}} get +1/+1/+1 for the rest of the game, and give +2 {{HP}} to an ally monster.',
+			['card-name-' + (customCardsStart+2)] : 'Chibi Yuri',
+			['card-'+ (customCardsStart+2)] : '{{KW:MAGIC}}: All {{TRIBE:CHIBI|2}} get +1/+1/+1 for the rest of the game, and give +2 {{ATK}} to an ally monster.',
+			['card-name-' + (customCardsStart+3)] : 'Chibi Monika',
+			['card-'+ (customCardsStart+3)] : '{{KW:MAGIC}}: All {{TRIBE:CHIBI|2}} get +1/+1/+1 for the rest of the game, and add a copy of all other ally {{TRIBE:CHIBI|2}} to your hand.'
 		}
 } );
 
