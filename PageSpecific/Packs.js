480a1ecd355d15a6eec631871f965c18bf6adf90
CMD-God <48065803+CMD-God@users.noreply.github.com>
2021-06-06 09:31:42 +0300

    CSS is working!

--- PageSpecific/Packs.js
+++ PageSpecific/Packs.js
@@ -50,9 +50,9 @@ var packs_data = [
 
 function GenerateBuyRow(pack_data) {
 	return `
-		<input type="number" id="PrettyCards_PackBuyCount" pack_id="${pack_data.code_id}" value="1" min="1" pattern="[0-9]">
-		x <button id="PrettyCards_PackGBuy" pack_id="${pack_data.code_id}">Buy (<span id="PrettyCards_PackGPrice" pack_id="${pack_data.code_id}">100</span> <img src="images/icons/gold.png" class="height-16">)</button>
-		 <button id="PrettyCards_PackUcpBuy" pack_id="${pack_data.code_id}">Buy (<span class="ucp" id="PrettyCards_PackUcpPrice" pack_id="${pack_data.code_id}">10</span> UCP)</button>
+		<input type="number" class="PrettyCards_PackBuyCount" pack_id="${pack_data.code_id}" value="1" min="1" pattern="[0-9]">
+		x <button class="PrettyCards_PackGBuy" pack_id="${pack_data.code_id}">Buy (<span id="PrettyCards_PackGPrice" pack_id="${pack_data.code_id}">100</span> <img src="images/icons/gold.png" class="height-16">)</button>
+		 <button class="PrettyCards_PackUcpBuy" pack_id="${pack_data.code_id}">Buy (<span class="ucp" id="PrettyCards_PackUcpPrice" pack_id="${pack_data.code_id}">10</span> UCP)</button>
 	`;
 }
 
@@ -92,11 +92,11 @@ if (settings.packs.value() && underscript.onPage('Packs')) {
 	DeleteUglyPage();
 	
 	mainContent.innerHTML += `
-		<div id="PrettyCards_PacksRow">
+		<div class="PrettyCards_PacksRow">
 			<div class="PrettyCards_PackCell" pack_id="Pack"></div>
 			<div class="PrettyCards_PackCell" pack_id="DRPack"></div>
 		</div>
-		<div id="PrettyCards_PacksRow">
+		<div class="PrettyCards_PacksRow">
 			<div class="PrettyCards_PackCell" pack_id="ShinyPack"></div>
 			<div class="PrettyCards_PackCell" pack_id="SuperPack"></div>
 			<div class="PrettyCards_PackCell" pack_id="FinalPack"></div>
@@ -114,6 +114,8 @@ if (settings.packs.value() && underscript.onPage('Packs')) {
 	//const my_css = GM_getResourceText("PACKS_CSS");
 	//console.log("CSS Loaded: ", my_css);
     //GM_addStyle(my_css);
+	
+	loadCSSFromLink("https://cdn.jsdelivr.net/gh/CMD-God/prettycards/css/Packs.css");
 	
 }
 
