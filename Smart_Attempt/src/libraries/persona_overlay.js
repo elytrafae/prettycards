b1a4061067dc685ee06707728b17aa0b36688699
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-03-11 19:23:50 +0200

    Persona Overlay Demo Done. It looks horrible!

--- Smart_Attempt/src/libraries/persona_overlay.js
+++ Smart_Attempt/src/libraries/persona_overlay.js
@@ -6,6 +6,21 @@ PrettyCards_plugin.events.on("PrettyCards:onPageLoad", function() {
     utility.loadCSSFromGH("PersonaOverlay");
 })
 
+function Linearizer(sin) {
+    if (sin > Math.PI/2) {
+        return 2-Math.sin(sin);
+    }
+    return Math.sin(sin);
+}
+
+PrettyCards_plugin.events.on("getMonsterPlayed", function(data) {
+    var card = JSON.parse(data.card);
+    console.log("PLAYED: ", card);
+    if (card.rarity == "LEGENDARY" || card.rarity == "DETERMINATION") {
+        CreateOverlay(card, null);
+    }
+})
+
 function CreateOverlay(card, side) {
     var border = $('<div class="PrettyCards_PersonaBorder"></div>');
     var back = $('<div class="PrettyCards_PersonaBack"></div>');
@@ -13,8 +28,24 @@ function CreateOverlay(card, side) {
     var image = $(`<img src="images/cards/${card.image}.png" class="PrettyCards_PersonaImage">`);
     back.append(image);
 
+    const start = 200;
+    const finish = -100;
+    const duration = 3000;
+
     image.css("left", "200%");
-    image.animate({"left": "-100%"}, 1000, "swing");
+    image.animate({"step": duration.toString()}, {
+        duration: duration,
+        easing: "linear",
+        step : function(now, tween) {
+            console.log(now, tween);
+            var val = (start + Linearizer(now/duration*Math.PI)/2*(finish-start) );
+            console.log(val, Math.sin(now/duration*Math.PI));
+            image.css("left", val + "%" );
+        },
+        complete: function() {
+            border.remove();
+        }
+    });
 
     $("body").append(border);
 }
