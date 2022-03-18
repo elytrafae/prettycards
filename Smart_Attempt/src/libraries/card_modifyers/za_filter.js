660d56e0a8a9a551bc915762026da4131391cf7c
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-03-18 17:28:08 +0200

    All Rarities Checkbox

--- Smart_Attempt/src/libraries/card_modifyers/za_filter.js
+++ Smart_Attempt/src/libraries/card_modifyers/za_filter.js
@@ -1,7 +1,29 @@
+import { PrettyCards_plugin, settings } from "../underscript_checker";
 
-const icon = "";
+const icon = "https://github.com/CMD-God/prettycards/raw/master/img/RarityIcons/ALL_RARITIES.png";
+const rarityInputList = ["baseRarityInput", "commonRarityInput", "rareRarityInput", "epicRarityInput", "legendaryRarityInput", "determinationRarityInput", "tokenRarityInput", "baseGenInput"];
 
-if (window.underscript.onPage("Crafting")) {
-    var label = $('<label><input type="checkbox" id="everyRarityInput"> <img style="height: 24px;" src="' + filter.icon + '" alt=""></label>');
+settings.za_filter = PrettyCards_plugin.settings().add({
+	'key': 'za_filter',
+	'name': 'Enable "Toggle All Rarities" button', // Name in settings page
+	'note': "Also known as the \"Za Filter\".",
+	'type': 'boolean',
+	'refresh': false, // true to add note "Will require you to refresh the page"
+	'default': true, // default value
+});
+
+function allRarityUpdate(e) {
+    var checked = $("#everyRarityInput").prop("checked");
+    for (var i=0; i < rarityInputList.length; i++) {
+        $("#" + rarityInputList[i]).prop("checked", checked);
+    }
+    window.applyFilters();
+    window.showPage(0);
+}
+
+if (window.underscript.onPage("Crafting") && settings.za_filter.value()) {
+    var label = $('<label><input type="checkbox" id="everyRarityInput"> <img style="height: 24px;" src="' + icon + '" alt="Check all rarities"></label>');
+    label.find("input").change(allRarityUpdate);
 	$("#baseGenInput").parent().parent().append(label);
 }
+
