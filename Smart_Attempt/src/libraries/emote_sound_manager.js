aab4657454bd4bac611d88db2327061290014a78
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-06-06 12:22:06 +0300

    CSS Upload

--- Smart_Attempt/src/libraries/emote_sound_manager.js
+++ Smart_Attempt/src/libraries/emote_sound_manager.js
@@ -6,7 +6,7 @@ settings.emote_sounds = PrettyCards_plugin.settings().add({
 	'key': 'emote_sounds',
 	'name': 'Enable Emote Sounds', // Name in settings page
 	'type': 'boolean',
-	'refresh': false, // true to add note "Will require you to refresh the page"
+	'refresh': true, // true to add note "Will require you to refresh the page"
 	'default': true, // default value
 });
 
@@ -14,7 +14,6 @@ var emoteAudio = new Audio();
 emoteAudio.volume = 0.7;
 
 function playEmoteSound(emote) {
-    console.log("PLAYING FOR EMOTE", emote);
     emoteAudio.pause();
     emoteAudio.src = `https://github.com/CMD-God/prettycards/raw/master/audio/emotes/${emote}.ogg`;
     emoteAudio.play();
@@ -22,7 +21,6 @@ function playEmoteSound(emote) {
 
 if (window.underscript.onPage("Game") || window.underscript.onPage("Spectate")) {
     PrettyCards_plugin.events.on("getEmote:before", function(data) {
-        console.log("getEmote EVENT", data);
         var isYou = window.userId == data.idUser;
         var image = data.emoteImage;
         if (window.gameEmotesEnabled) { // This if chain is the same as Onu's, for the sake of integrity.
@@ -32,5 +30,19 @@ if (window.underscript.onPage("Game") || window.underscript.onPage("Spectate")) 
                 playEmoteSound(image);
             }
         }
+    })
+}
+
+if (window.underscript.onPage("CosmeticsShop")) {
+    PrettyCards_plugin.events.on("PrettyCards:onPageLoad", function() {
+        $(".emote-bordered").each(function(i, e) {
+            var url = new URL(e.src).pathname;
+            const emote = url.substring(15, url.length - 4); // Crops the image name from the src\
+            var speaker = $(`<div class="glyphicon glyphicon-volume-up PrettyCards_EmoteSoundPlayer"></div>`)[0];
+            speaker.onclick = onclick = function() {
+                playEmoteSound(emote);
+            }
+            e.parentElement.appendChild(speaker);
+        });
     })
 }
