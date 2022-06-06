4e3a1b48884bd0ffc315a8c724cc00090b65d3ed
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-06-06 11:26:35 +0300

    Audio Upload

--- Smart_Attempt/src/libraries/emote_sound_manager.js
+++ Smart_Attempt/src/libraries/emote_sound_manager.js
@@ -1,5 +1,5 @@
-import { PrettyCards_plugin, settings } from "../underscript_checker";
-import { utility } from "../utility";
+import { PrettyCards_plugin, settings } from "/src/libraries/underscript_checker";
+import { utility } from "/src/libraries/utility";
 
 
 settings.emote_sounds = PrettyCards_plugin.settings().add({
@@ -11,20 +11,25 @@ settings.emote_sounds = PrettyCards_plugin.settings().add({
 });
 
 var emoteAudio = new Audio();
+emoteAudio.volume = 0.7;
 
 function playEmoteSound(emote) {
+    console.log("PLAYING FOR EMOTE", emote);
     emoteAudio.pause();
+    emoteAudio.src = `https://github.com/CMD-God/prettycards/raw/master/audio/emotes/${emote}.ogg`;
+    emoteAudio.play();
 }
 
 if (window.underscript.onPage("Game") || window.underscript.onPage("Spectate")) {
-    PrettyCards_plugin.events.on("getEmote", function(data) {
+    PrettyCards_plugin.events.on("getEmote:before", function(data) {
+        console.log("getEmote EVENT", data);
         var isYou = window.userId == data.idUser;
         var image = data.emoteImage;
         if (window.gameEmotesEnabled) { // This if chain is the same as Onu's, for the sake of integrity.
-            if (isYou && !window.youDoingEmote) {
-                
+            if (isYou && !window.youDoingEmote) { // Left these separated like this so I can potentially do different things for enemies and allies easier.
+                playEmoteSound(image);
             } else if (!isYou && !window.enemyDoingEmote && !window.enemyMute) {
-                
+                playEmoteSound(image);
             }
         }
     })
