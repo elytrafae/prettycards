5034dc8897755144ec70b1b42dbde8fec4dd65a4
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-06-25 16:34:36 +0300

    More Translation Stuff

--- Smart_Attempt/src/libraries/card_modifyers/switch_highlight.js
+++ Smart_Attempt/src/libraries/card_modifyers/switch_highlight.js
@@ -11,6 +11,36 @@ function isElementSwitchCard(ele) {
     return ele.find(".PrettyCards_SwitchHighlight_Cyan").length > 0 || ele.find(".PrettyCards_SwitchHighlight_Red").length > 0;
 }
 
+function doYouHaveEndgame() {
+    var art = $(`#yourArtifacts img[artifactid="47"]`);
+    return art.length > 0;
+}
+
+function isCardElementAPiece() {
+    var tribe = $(`.cardTribes img[src=images/tribes/PIECE.png]`);
+    return tribe.length > 0;
+}
+
+function setSwitchAreaAndCard(ele, state = 0) {
+    $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Cyan");
+    $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Red");
+    $("#yourSide").removeClass("PrettyCards_SwitchHighlight_EndgameBoard");
+    var descDiv = ele.find(".cardDesc > div");
+    var id = ele.attr("fixedId");
+    if (state == 1) {
+        $("#yourSide").addClass("PrettyCards_SwitchHighlight_SwitchBoard_Cyan");
+        descDiv.html($.i18n(`card-${id}-cyan`));
+    } else if (state == 2) {
+        $("#yourSide").addClass("PrettyCards_SwitchHighlight_SwitchBoard_Red");
+        descDiv.html($.i18n(`card-${id}-red`));
+    } else if (state == 3) {
+        $("#yourSide").addClass("PrettyCards_SwitchHighlight_EndgameBoard");
+        descDiv.html($.i18n(`card-${id}-both`));
+    } else {
+        descDiv.html($.i18n(`card-${id}`));
+    }                
+}
+
 function gameSetup() {
     console.log("GAME SETUP!");
     var oldMakeDraggable = window.makeDraggable;
@@ -25,47 +55,77 @@ function gameSetup() {
             });
             $e.on("dragstop", function() {
                 $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard");
-                $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Cyan");
-                $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Red");
+                setSwitchAreaAndCard($e, 0);
             });
             window.enableDraggable();
         })
+    }
 
-        var oldUpdateDroppables = window.updateDroppables;
-        window.updateDroppables = function($card) {
-            oldUpdateDroppables($card);
-            if ($card.hasClass('canPlay')) {
-                if ($card.hasClass('monster')) { //&& $card.find(".PrettyCards_SwitchLeft").length > 0) { // Check disabled for testing purposes.
-                    $('.droppableMonster:not(:has(.monster))').on( "dropover", function( event, ui ) {
-                        var $card = ui.draggable;
-                        if (!isElementSwitchCard($card)) {return;}
-                        var position = event.target.getAttribute("x");
-                        console.log("DROPOVER", $card, position);
-                        if (position < 2) {
-                            $("#yourSide").addClass("PrettyCards_SwitchHighlight_SwitchBoard_Cyan");
-                            $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Red");
-                        } else {
-                            $("#yourSide").addClass("PrettyCards_SwitchHighlight_SwitchBoard_Red");
-                            $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Cyan");
-                        }
-                    });
+    var oldUpdateDroppables = window.updateDroppables;
+    window.updateDroppables = function($card) {
+        oldUpdateDroppables($card);
+        if ($card.hasClass('canPlay')) {
+            if ($card.hasClass('monster')) { //&& $card.find(".PrettyCards_SwitchLeft").length > 0) { // Check disabled for testing purposes.
+                $('.droppableMonster:not(:has(.monster))').on( "dropover", function( event, ui ) {
+                    var $card = ui.draggable;
+                    if (!isElementSwitchCard($card)) {return;}
+                    var position = Number(event.target.getAttribute("x"));
+                    if (isNaN(position)) {return;}
+                    //console.log("DROPOVER", $card, position);
+                    if (doYouHaveEndgame()) {
+                        setSwitchAreaAndCard($card, 3);
+                    } else {
+                        setSwitchAreaAndCard($card, (position < 2) ? 1 : 2);
+                    }
+                });
 
-                    $('.droppableMonster:not(:has(.monster))').on("dropout", function( event, ui ) { // I hate how this reminded me of my childhood . . .
-                        $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Red");
-                        $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Cyan");
-                        console.log("DROPOUT", event, ui);
-                    });
+                $('.droppableMonster:not(:has(.monster))').on("dropout", function( event, ui ) { // I hate how this reminded me of my childhood . . .
+                    var $card = ui.draggable;
+                    setSwitchAreaAndCard($card, 0);
+                    //console.log("DROPOUT", event, ui);
+                });
 
-                    $('.droppableMonster:not(:has(.monster))').on("drop", function(event, ui) {
-                        $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard");
-                        $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Cyan");
-                        $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard_Red");
-                    });
-                }
-                // So far, there are no Switch Spells. I'll worry about this later.
+                $('.droppableMonster:not(:has(.monster))').on("drop", function(event, ui) {
+                    var $card = ui.draggable;
+                    $("#yourSide").removeClass("PrettyCards_SwitchHighlight_SwitchBoard");
+                    setSwitchAreaAndCard($card, 0);
+                });
             }
+            // So far, there are no Switch Spells. I'll worry about this later.
         }
     }
+
+    PrettyCards_plugin.events.on("appendCard()", function(data) {
+        var card = data.card;
+		var element = data.element;
+        var id = card.fixedId || card.id;
+        //console.log("ASSIGN FIXED ID", card, id);
+        element.attr("fixedId", id);
+    })
+
+    /*
+    PrettyCards_plugin.events.on("appendCard()", function(data) {
+        var card = data.card;
+		var element = data.element;
+        if (!isElementSwitchCard(element)) {return;}
+        var id = card.fixedId | card.id;
+        var regularText = $.i18n(`card-${id}`);
+        var cyanKey = `card-${id}-cyan`;
+        var redKey = `card-${id}-red`;
+        var cyanText = $.i18n(cyanKey);
+        var redText = $.i18n(redKey);
+        if (cyanText == cyanKey) {
+            cyanText = regularText;
+        }
+        if (redText == redKey) {
+            redText = regularText;
+        }
+
+        element.find(".cardDesc").append(`<div class="PrettyCards_SwitchHighlight_CyanDesc">${cyanText}</div><div class="PrettyCards_SwitchHighlight_CyanDesc">${cyanText}</div>`);
+
+    })
+    */
+
 }
 
 PrettyCards_plugin.events.on("PrettyCards:onPageLoad", function() {
