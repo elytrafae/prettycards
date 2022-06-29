54254c97978fcb1b46edb5a29ccf436b09945d1e
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-06-29 12:03:49 +0300

    2.11.3b5

--- Smart_Attempt/dist/prettycards.meta.js
+++ Smart_Attempt/dist/prettycards.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        PrettyCards
-// @version     2.11.3b4
+// @version     2.11.3b5
 // @author      CMD_God
 // @description Make (parts of) Undercards look fabulous!
 // @homepage    https://github.com/CMD-God/prettycards
