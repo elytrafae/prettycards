1a443d3b7287890bfdb13764cc5ffb65f079cdf0
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-09-11 16:33:51 +0300

    Hidden Hate

--- Smart_Attempt/src/page_specific/custom_cards.js
+++ Smart_Attempt/src/page_specific/custom_cards.js
@@ -9,7 +9,7 @@ import {LoadFont, ListenForWhenAllFontsAreLoaded} from "/src/libraries/font_load
 
 import {} from "/src/libraries/card_modifyers/custom_cards/custom_cards_ddlc_v2.js";
 import {} from "/src/libraries/card_modifyers/custom_cards/custom_cards_switch.js";
-import {} from "/src/libraries/card_modifyers/custom_cards/custom_cards_hate.js";
+// import {} from "/src/libraries/card_modifyers/custom_cards/custom_cards_hate.js";
 import {} from "/src/libraries/card_modifyers/custom_cards/custom_cards_uco.js";
 
 import { createFloatingSoul } from "../libraries/floating_souls";
