096aa9cc88dfb66bb4c7ea95e5f27423c0091e09
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-09-14 16:43:37 +0300

    Ajax Hooker and EN Update

--- Smart_Attempt/src/index.js
+++ Smart_Attempt/src/index.js
@@ -8,6 +8,7 @@ import {} from "/src/libraries/custom_page_base.js";
 
 // The Event ensurer MUST be required AFTER underscript stuff, but BEFORE anything else that might require it!
 import {ExecuteWhen} from "/src/libraries/pre_load/event_ensure.js";
+import {} from "/src/libraries/ajax_event_hooker.js";
 
 // This is here to make sure the background settings are above the Miscallenious ones.
 import {} from "/src/libraries/background/background_manager.js";
---
+++ Smart_Attempt/src/libraries/ajax_event_hooker.js
@@ -0,0 +1,21 @@
+
+import {PrettyCards_plugin, settings} from "/src/libraries/underscript_checker.js";
+
+PrettyCards_plugin.events.on("PrettyCards:onPageLoad", function() {
+    window.$(document).ajaxComplete((event, xhr, s) => {
+        console.log(event, xhr, s);
+        if (s.url === "CardSkinsConfig?action=shop") {
+            var old = s.success;
+            s.success = function(data) {
+                console.log("HIGHJACKED!", data);
+                old(data);
+            }
+            PrettyCards_plugin.events.emit.singleton("PrettyCards:onCardSkinShopConfig");
+            return;
+        }
+        if (s.url === "CardSkinsConfig") {
+            PrettyCards_plugin.events.emit("PrettyCards:onCardSkinBought"); // Sadly, I dunno how to check which one :(
+        }
+        // Do stuff here
+    });
+});
