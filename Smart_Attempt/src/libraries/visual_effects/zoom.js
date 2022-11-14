2968aa0287242fee58fdce8337eba98a9952d96f
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-11-14 11:32:53 +0200

    A little bit of work on the zoom manager

--- Smart_Attempt/src/libraries/visual_effects/zoom.js
+++ Smart_Attempt/src/libraries/visual_effects/zoom.js
@@ -1,4 +1,6 @@
 
+import { PrettyCards_plugin, prettycards } from "./underscript_checker";
+
 // Will need to animate this and code a way to zoom in on a specific element.
 
 class ZoomManager {
@@ -11,7 +13,13 @@ class ZoomManager {
     }
 
     zoomScreen(xZoom, yZoom) {
-        window.$("")
+        zoomElement(xZoom, yZoom, $("body"));
     }
 
 }
+
+var zoomManager = new ZoomManager();
+
+prettycards.zoomManager = zoomManager;
+
+export {zoomManager};
