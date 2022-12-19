f3868106444614a0a20c29294427cd13845951bd
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-12-19 14:18:27 +0200

    Wording Fix

--- Smart_Attempt/src/libraries/card_modifyers/custom_cards/custom_cards_hate.js
+++ Smart_Attempt/src/libraries/card_modifyers/custom_cards/custom_cards_hate.js
@@ -159,6 +159,7 @@ PrettyCards_plugin.events.on("PrettyCards:customCards", function () {
 	});
 
     var aimless_vessel = c.newCard({
+		soul: soul,
         name: "{{PLURAL:$1|Aimless Vessel|Aimless Vessel}}",
         image: "Aimless_Vessel",
         cost: 5,
@@ -169,7 +170,7 @@ PrettyCards_plugin.events.on("PrettyCards:customCards", function () {
         rarity: "TOKEN"
     });
 
-    numbing.setDescription(`Kill a non-DT ally monster costing 3 or more and add an ${aimless_vessel.me()} to your hand with same stats.`);
+    numbing.setDescription(`Kill a non-{{RARITY:DETERMINATION}} ally monster costing 3 or more and add an ${aimless_vessel.me()} to your hand with same stats.`);
 
 	c.newCard({
 		soul: soul,
