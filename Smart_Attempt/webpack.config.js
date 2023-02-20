e2cb65b66bfdf71893c3d5506f379888346b252d
CMD-God <48065803+CMD-God@users.noreply.github.com>
2023-02-20 17:42:54 +0200

    Update webpack.config.js

--- Smart_Attempt/webpack.config.js
+++ Smart_Attempt/webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
         exclude: 'https://*.undercards.net/*/*',
         //updateURL: `https://unpkg.com/${package.name}/dist/${package.name}.meta.js`,
         updateURL: `https://github.com/CMD-God/${package.name}/releases/latest/download/${package.name}.user.js`,
-        downloadURL: `https://unpkg.com/${package.name}/dist/${package.name}.user.js`,
+        //downloadURL: `https://unpkg.com/${package.name}/dist/${package.name}.user.js`,
+		downloadURL: `https://github.com/CMD-God/prettycards/releases/latest/download/prettycards.user.js`,
         require: [],
         grant: 'none',
         "run-at" : "document-idle" 
