11eb493501e399adb682f09513097b5c5720aa12
CMD-God <48065803+CMD-God@users.noreply.github.com>
2023-04-06 19:13:45 +0300

    UCO Card Ids Update

--- Smart_Attempt/src/libraries/card_modifyers/card_hover_shiny_fix.js
+++ Smart_Attempt/src/libraries/card_modifyers/card_hover_shiny_fix.js
@@ -9,8 +9,7 @@ var shinyHoverFix = addSetting({
     'note': `Hovering over a card's name to see what it is SHOULD now have proper shiny status based on the parent card.`,
     'type': 'boolean',
     'refresh': true, // true to add note "Will require you to refresh the page"
-    'default': false,//true, // default value
-    'hidden': true,
+    'default': true,//true, // default value
     'category': "card"
 });
 
