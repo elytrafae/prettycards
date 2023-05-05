18e29e4a5cf2c4a3d28b286368f9289e2a44828f
CMD-God <48065803+CMD-God@users.noreply.github.com>
2023-05-05 13:20:27 +0300

    Badge Update!

--- Smart_Attempt/src/libraries/chat/custom_chat_badges.js
+++ Smart_Attempt/src/libraries/chat/custom_chat_badges.js
@@ -8,11 +8,27 @@ const PC_CONTRIB_BADGE = {
     icon: "PC_Contrib"
 }
 
+var custom_badges = [PC_CONTRIB_BADGE];
+
 function processMessage(message) {
     var user = message.user;
     user.groups.push(PC_CONTRIB_BADGE);
     user.mainGroup = PC_CONTRIB_BADGE;
-    console.log(message);
+}
+
+function correctCustomIcons(message, idRoom, isPrivate) {
+    var room = isPrivate ? 'chat-private-' + idRoom : 'chat-public-' + idRoom;
+    var $chat = $('#' + room);
+    var icons = $chat.find("#message-" + message.id).find(".groupIcon");
+    console.log("ICONS", icons);
+    for (var i=0; i < icons.length; i++) {
+        var icon = icons[i];
+        var title = icon.getAttribute("title");
+        var badge = custom_badges.find((badge) => badge.name == title);
+        if (badge) {
+            icon.src = `https://raw.githubusercontent.com/CMD-God/prettycards/master/img/ChatBadges/${badge.icon}.png`;
+        }
+    }
 }
 
 function processChatHistoryEvent(data) {
@@ -26,7 +42,6 @@ function processChatHistoryEvent(data) {
 function processChatMessageEvent(data) {
     var message = JSON.parse(data.chatMessage);
     processMessage(message);
-    console.log("MESSAGE2", message);
     data.chatMessage = JSON.stringify(message); // I hate that I have to do stuff like this >~<
 }
 
@@ -37,9 +52,10 @@ PrettyCards_plugin.events.on("PrettyCards:onPageLoad", () => {
     window.appendMessage = (chatMessage, idRoom, isPrivate) => {
         processMessage(chatMessage);
         oldfn(chatMessage, idRoom, isPrivate);
+        correctCustomIcons(chatMessage, idRoom, isPrivate);
     }
 
 })
 
-PrettyCards_plugin.events.on("preChat:getHistory preChat:getPrivateHistory", processChatHistoryEvent);
-PrettyCards_plugin.events.on("preChat:getMessage preChat:getPrivateMessage", processChatMessageEvent);
+//PrettyCards_plugin.events.on("preChat:getHistory preChat:getPrivateHistory", processChatHistoryEvent);
+//PrettyCards_plugin.events.on("preChat:getMessage preChat:getPrivateMessage", processChatMessageEvent);
