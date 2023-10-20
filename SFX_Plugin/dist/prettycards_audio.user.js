658436eea308226254dac7c8ad9fdada3ef039fa
CMD-God <48065803+CMD-God@users.noreply.github.com>
2023-10-20 13:13:07 +0300

    Version change (almost forgot)

--- SFX_Plugin/dist/prettycards_audio.user.js
+++ SFX_Plugin/dist/prettycards_audio.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        PrettyCards Audio
-// @version     1.1.0
+// @version     1.1.1
 // @author      CMD_God
 // @description Add vibrant sounds to an Undercards match!
 // @homepage    https://github.com/CMD-God/prettycards
