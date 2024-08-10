d5431ab484c68a75962f10a7e400f420dd02b966
CMD-God <48065803+elytrafae@users.noreply.github.com>
2024-08-10 13:37:50 +0300

    Removed stuff that was moved to the other repo

--- OldVersion/PageSpecific/GamesList.js
+++
@@ -1,9 +0,0 @@
-
-underscript.onPage("GamesList", function () {
-	if (settings.replace_soul_selection.value()) {
-		var soulSelect = document.getElementById("customDecks");
-	  var div = CustomizeSoulSelectObj(soulSelect, localStorage.getItem("customDeck"), 3);
-		div.style.display = "inline-block";
-	}
-    //document.getElementById("soulInfo").style.display = "none";
-})
--- quote_major_changes/RealKnifeCard.java
+++
@@ -1,5 +0,0 @@
-package quote_major_changes;
-
-public class RealKnifeCard extends Card {
-
-}
