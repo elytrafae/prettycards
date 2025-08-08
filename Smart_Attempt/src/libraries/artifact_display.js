61036c6694cd110e8b9951c4607bd9e29ca9c7c1
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-08-08 08:50:41 +0300

    Fixes
    - Card rarity icons now have a UTY option (in previous commit, I just didn't notice)
    - More fixes for some artifacts showing up as purchasable for PrettyCards, when they weren't

--- Smart_Attempt/src/libraries/artifact_display.js
+++ Smart_Attempt/src/libraries/artifact_display.js
@@ -149,7 +149,8 @@ class ArtifactDisplay {
 					/**@type {String} */
 					var nameId = row.children[1].getAttribute("data-i18n");
 					var priceEntity = row.children[4].firstChild;
-					if (priceEntity) { // Fix for artifacts page containing all artifacts, and not just ones you can buy
+					var buyCell = row.children[5];
+					if (priceEntity && buyCell.querySelector("form")) { // Fix for artifacts page containing all artifacts, and not just ones you can buy
 						var price = parseInt(row.children[4].firstChild.innerText);
 						var id = parseInt(lastOf(nameId.split('-')));
 						//console.log(row, nameId, id);
